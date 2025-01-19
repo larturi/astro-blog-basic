2ae8d32d59ad0d76c0b839b36a571b8054a64745
Leandro Arturi <lea.arturi@gmail.com>
2025-01-19 11:51:39 -0300

    Images

---
+++ src/content/config.ts
@@ -0,0 +1,23 @@
+import { defineCollection, z } from 'astro:content'
+
+const blogCollection = defineCollection({
+  type: 'content',
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      description: z.string(),
+      // image: image().refine((img) => img.width < 1200, {
+      //   message: 'Image width must be less than 1200px'
+      // }),
+      image: z.string(),
+
+      author: z.string(),
+
+      tags: z.array(z.string())
+    })
+})
+
+export const collections = {
+  blog: blogCollection
+}
